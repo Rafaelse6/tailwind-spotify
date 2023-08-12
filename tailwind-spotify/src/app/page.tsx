fd82d069d2ba042a00ee839a5edde979fa189bb6
Rafael Santos <rafa.05125@gmail.com>
2023-08-11 21:18:37 -0300

    Playlists side navigation

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
             <div className="w-3 h-3 bg-green-500 rounded-full"></div>
           </div>
-          
+
           <nav className="space-y-6 mt-10">
             <a
               href=""
@@ -30,6 +30,21 @@ export default function Home() {
               className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
             >
               <Library /> Your Library
+            </a>
+          </nav>
+
+          <nav className="mt-10 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Mix Anos 2010
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Daily Mix 5
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              日本語
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Caminho Diário
             </a>
           </nav>
         </aside>
